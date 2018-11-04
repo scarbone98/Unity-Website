679b858e7c0d044b77596f1e528bf7a1e9a8e748
Samuel Carbone <sammy@Samuels-MacBook-Pro.local>
2018-11-03 22:09:56 -0400

    Updates

--- src/Views/RootPage.js
+++ src/Views/RootPage.js
@@ -10,6 +10,7 @@ class RootPage extends Component {
                 <div className="links-container" style={{display: 'flex', flexDirection: 'column'}}>
                     <Link to="/feedback" style={{margin: 5}}>Add Feedback</Link>
                     <Link to="/announcements" style={{margin: 5}}>Create Announcement</Link>
+                    <Link to="/viewfeedback" style={{margin: 5}}>View Feedback</Link>
                 </div>
             </div>
         );
